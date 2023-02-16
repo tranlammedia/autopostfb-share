544a3523789c6598b6c49295d7fc90de941e9ab5
tranlam <tranlam@Debian>
2023-02-16 10:23:32 +0700

    node-fetch

--- config/tokensData.js
+++ config/tokensData.js
@@ -6,7 +6,7 @@
 // nếu không thấy, có thể liên hệ mình theo thông tin trong file huongdan.txt
 
 exports.tokens = {
-    page1: 'EAAKcMOZCuOc...FvU5413IMu',
-    page2: 'EAAG3bb04804...qBVFbQwufegZDZD' 
+    page1: 'EAAKcMOZCuOcwBAEdEZB3sj2P1q6Y1pyXFJJo4nnp6QBR1F8uphIFyW1FdB6N7XX6ISl9oG15iWYXGEEvFdHPtbn6TJEUhTrMt5dbZAe1nCzCO0qXsLslrp25nqmqXLsE63SndhB8G9UjXdfyovfLquIGlZBqOuB9VsSZASDWKxyFvU5413IMu',
+    page2: 'EAAG3bb04804BAO5XhdDdrHOCx5jE69XEFSy3ZAVgfSfdHq9L0g6fbKv5Ka9vGPoii8wmit5cBqbqkBizfvVhAmIHdfWSL3VmZAkREhkuhHRIXlqtsLMmtZBgrgdsqSTjDha0WSWFEr7z1eqB7XFVgPYDdSmN5IdDob7MiCeMrI7zm5TqD1HFj2kApr2uxLqBVFbQwufegZDZD' 
 }
 
