6b70bef0a42be6ae65867674d35ff93b0666e246
Tran Lam <79444847+tranlammedia@users.noreply.github.com>
2023-02-16 11:15:11 +0700

    Update pageToGroupsData.js

--- config/pageToGroupsData.js
+++ config/pageToGroupsData.js
@@ -1,6 +1,10 @@
 // fromPage: tên page trùng khớp với tên bên file tokensData.js
 // toGroup: nơi page sẽ post, nếu 'me', page sẽ post lên tường của nó
 
+
+// trước khi thêm vào, page phải join trước vào group
+// nên join group không kiểm duyệt
+
 exports.pageToGroups= [
     {
         fromPage: 'page1',
@@ -13,5 +17,5 @@ exports.pageToGroups= [
     {
         fromPage: 'page1',
         toGroup: '1425275367755509'
-    },
+    }
 ]
